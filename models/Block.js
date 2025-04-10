2b7851f9ca449af60b0964c8696fced51ab7120e
KhachaWalid <walidkhacha123@gmail.com>
2025-04-10 11:00:19 +0100

    [Models] Update Block model with room relationship and cascade delete

--- models/Block.js
+++ models/Block.js
@@ -1,10 +1,31 @@
 const mongoose = require("mongoose");
 
 const BlockSchema = new mongoose.Schema({
-    name: { type: String, required: true, unique: true }, 
-    numberOfFloors: { type: Number, required: true }, 
-    numberOfRooms: { type: Number, required: true }, 
-    roomsPerFloor: [{ type: Number, required: true }] 
+    name: { 
+        type: String, 
+        required: true, 
+        unique: true 
+    },
+    floors: { 
+        type: Number, 
+        required: true 
+    },
+    roomsPerFloor: [{ 
+        type: Number, 
+        required: true 
+    }],
+    totalRooms: { 
+        type: Number,
+        default: function() {
+            return this.roomsPerFloor.reduce((sum, num) => sum + num, 0);
+        }
+    }
+});
+
+// Middleware to delete associated rooms when a block is removed
+BlockSchema.pre('remove', async function(next) {
+    await Room.deleteMany({ block: this._id });
+    next();
 });
 
 module.exports = mongoose.model("Block", BlockSchema);
