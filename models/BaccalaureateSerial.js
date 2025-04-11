7d1823c1f9cb33332acaa2e32b59fe0e50f3ebc2
KhachaWalid <walidkhacha123@gmail.com>
2025-04-11 15:20:31 +0100

    [Models] added first and last name

--- models/BaccalaureateSerial.js
+++ models/BaccalaureateSerial.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 
 const BaccalaureateSerialSchema = new mongoose.Schema({
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
     serialNumber: { type: String, unique: true, required: true },
     secretNumber: { type: String, required: true },
     registrationFeesPaid: { type: Boolean, default: false } 
